34cc3a76fc41d5a5bc31ba6daca75631fe3b690e
Siwanper <15091631746@qq.com>
2019-06-04 17:48:51 +0800

    111

---
+++ 1-spring-cloud-eureka/spring-cloud-eureka-2/src/main/java/com/swp/springcloudeureka2/SpringCloudEureka2Application.java
@@ -0,0 +1,13 @@
+package com.swp.springcloudeureka2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEureka2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEureka2Application.class, args);
+	}
+
+}
