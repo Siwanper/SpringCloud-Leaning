7eaf793b6116af4e5ccbf4c65635de2a42589b51
Siwanper <15091631746@qq.com>
2019-06-20 11:37:04 +0800

    111

--- 11-spring-cloud-gateway/spring-cloud-gateway/src/test/java/com/swp/springcloudgateway/SpringCloudGatewayApplicationTests.java
+++ 11-spring-cloud-gateway/spring-cloud-gateway/src/test/java/com/swp/springcloudgateway/SpringCloudGatewayApplicationTests.java
@@ -5,12 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.ZonedDateTime;
+import java.util.Date;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringCloudGatewayApplicationTests {
 
     @Test
     public void contextLoads() {
+        System.out.println(ZonedDateTime.now());
     }
 
 }
