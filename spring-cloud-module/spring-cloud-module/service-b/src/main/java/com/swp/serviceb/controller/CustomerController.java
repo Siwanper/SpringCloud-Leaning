f0afb9cd1b8377a43973bf3b01654560d4090748
Siwanper <15091631746@qq.com>
2019-07-04 14:16:55 +0800

    22

--- spring-cloud-module/spring-cloud-module/service-b/src/main/java/com/swp/serviceb/controller/CustomerController.java
+++ spring-cloud-module/spring-cloud-module/service-b/src/main/java/com/swp/serviceb/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.swp.serviceb.controller;
 import com.swp.serviceb.service.ProducerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @outhor ios
  * @create 2019-07-01 6:09 PM
  */
+@RefreshScope
 @RestController
 public class CustomerController {
 
