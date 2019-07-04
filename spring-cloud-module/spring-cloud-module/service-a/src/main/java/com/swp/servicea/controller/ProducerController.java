265abea37785375dd3b1ceb8982fff8cd88cf217
shiwanpeng <15091631746@163.com>
2019-07-04 22:57:11 +0800

    11

--- spring-cloud-module/spring-cloud-module/service-a/src/main/java/com/swp/servicea/controller/ProducerController.java
+++ spring-cloud-module/spring-cloud-module/service-a/src/main/java/com/swp/servicea/controller/ProducerController.java
@@ -1,5 +1,7 @@
 package com.swp.servicea.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @outhor ios
  * @create 2019-07-01 6:08 PM
  */
+@RefreshScope
 @RestController
 public class ProducerController {
 
+    @Value("${msg}")
+    private String msg;
+
     @RequestMapping("/hello")
     public String hello(@RequestParam(value = "name") String name){
-        return "hello " + name + ", i'm first producer";
+        return "hello " + name + ", " + msg + "'m first producer";
     }
 
 }
