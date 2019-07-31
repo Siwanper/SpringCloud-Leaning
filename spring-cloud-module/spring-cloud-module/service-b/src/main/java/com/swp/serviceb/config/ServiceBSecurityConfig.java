0a89249f379a0a2ddc0241f030fa45474eedbd89
Siwanper <15091631746@qq.com>
2019-07-31 09:46:00 +0800

    111

--- spring-cloud-module/spring-cloud-module/service-b/src/main/java/com/swp/serviceb/config/ServiceBSecurityConfig.java
+++ spring-cloud-module/spring-cloud-module/service-b/src/main/java/com/swp/serviceb/config/ServiceBSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.swp.serviceb.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 /**
  * 描述:
@@ -13,12 +16,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
  * @create 2019-07-26 12:18 PM
  */
 @Configuration
-@EnableWebSecurity
-public class ServiceBSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
+public class ServiceBSecurityConfig extends ResourceServerConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()// 设置放行的url
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                // 设置放行的url
                 .antMatchers("/actuator/**").permitAll()
                 .anyRequest().authenticated();
     }
