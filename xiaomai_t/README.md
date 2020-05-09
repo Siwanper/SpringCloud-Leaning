c9b9def6c1ba24628543e4d73b5e75eb9d93ff6c
shiwanpeng <15091631746@163.com>
2020-05-09 16:54:32 +0800

    11

--- xiaomai_t/README.md
+++ xiaomai_t/README.md
@@ -1,7 +1,7 @@
 
-#Swagger2 使用方法
+###Swagger2 使用方法
 
-##Maven配置
+####Maven配置
     <dependencies>
         <!--Swagger2 - RESTful API文档-->
         <dependency>
@@ -16,13 +16,13 @@
         </dependency>
     </dependencies>
     
-##配置文件 及 插件配置
+####配置文件 及 插件配置
 
 在启动类上 加@EableSwagger2 
 
 默认swagger2的界面地址是:项目的根路径/swagger-ui.html
 
-##注解API
+####注解API
 
 @Api：修饰整个类，描述Controller的作用
 
@@ -46,3 +46,30 @@
 
 @ApiImplicitParams：描述由多个 @ApiImplicitParam 注解的参数组成的请求参数列表
 
+### 单元测试
+
+1、单元测试的特性
+
++ 单元测试不应该依赖外部系统
++ 单元测试运行速度很快
++ 单元测试不应该造成测试环境的脏数据
++ 单元测试可以重复运行。
+
+2、单元测试怎么写
+
+数据库操作层、中间件依赖层、业务逻辑层，各自的单元测试各自写，互相不要有依赖。
+
+
++ dao层测试，使用H2进行测试，做独立的BaseH2Test，只对dao的测试
++ service层测试，依赖mockito框架，使用@RunWith(MockitoJUnitRunner.class)注解，就无需加载其他spring bean，具体用法
++ 对于依赖外部的中间件（例如redis、diamond、mq），在处理单测的时候要注意分开加载和测试，尤其是与dao的测试分开
+
+### TDD
+
+测试驱动开发(Test-Driven Development)。是敏捷开发中的一项核心实践和技术。
+
+TDD是在开发功能代码之前，先编写单元测试用例代码，测试代码确定需要编写什么产品代码。
+
+
+### Mockito 框架
+[参考链接](https://www.jianshu.com/p/ef282f186874)
--- xiaomai_t/auth/authentication-client/src/test/java/com/siwanper/authentication/AuthenticationClientApplicationTests.java
+++ xiaomai_t/auth/authentication-client/src/test/java/com/siwanper/authentication/AuthenticationClientApplicationTests.java
@@ -3,6 +3,7 @@ package com.siwanper.authentication;
 
 import com.siwanper.authentication.provider.AuthenticationProvider;
 import com.siwanper.authentication.service.impl.AuthService;
+import com.siwanper.core.entity.vo.Result;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,29 +15,32 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.lang.reflect.Field;
 
+import static org.mockito.Mockito.when;
+
 //MockitoJUnitRunner 用于提供单元测试运行的容器环境
 @RunWith(MockitoJUnitRunner.class)
 public class AuthenticationClientApplicationTests {
-
-	// 用于模拟待测试模块中依赖的外部组件, 也就是authService中 依赖bean
-	@Mock
-	private AuthenticationProvider authenticationProvider;
 
 	// 用于标识待测试组件
 	@InjectMocks
 	private AuthService authService;
 
-	private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJzaXdhbnBlciIsInNjb3BlIjpbInJlYWQiXSwib3JnYW5pemF0aW9uIjoic2l3YW5wZXIiLCJleHAiOjE1ODg5Mzg1OTQsImF1dGhvcml0aWVzIjpbIklUIl0sImp0aSI6Ijc4MTdkMTI4LTE0NTYtNDdiYy05YzhkLTNiM2JjNDcyYmNlNyIsImNsaWVudF9pZCI6InRlc3RfY2xpZW50In0.NgCOFrL5Xwdp7F8xTEiNfk1cumNIeAxO7tfF8LwUq7k";
+	// 用于模拟待测试模块中依赖的外部组件, 也就是authService中 依赖bean
+	@Mock
+	private AuthenticationProvider authenticationProvider;
+
+	private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJzaXdhbnBlciIsInNjb3BlIjpbInJlYWQiXSwib3JnYW5pemF0aW9uIjoic2l3YW5wZXIiLCJleHAiOjE1ODg5OTk0NTUsImF1dGhvcml0aWVzIjpbIklUIl0sImp0aSI6Ijc2ZGExNTA2LTVhOWQtNDkzOS04ZWVkLTYwM2EzNTAxNzZjNSIsImNsaWVudF9pZCI6InRlc3RfY2xpZW50In0.VJdSzxIT-tZdtl6S3Ppd916pZTM9lae0RxjIlDFKo3E";
 	private static final String BEARER = "Bearer ";
 
 	@Before
 	public void before() throws NoSuchFieldException, IllegalAccessException {
-		//用于初始化@Mock注解修饰的组件
-		MockitoAnnotations.initMocks(this);
 
 		authService = new AuthService();
 		setInstancePrivateField(authService, "signingKey", "123456");
 		setInstancePrivateField(authService, "ignoreUrls", "/oauth,/open");
+
+		//用于初始化@Mock注解修饰的组件，注意需要先创建AuthService对象，否则会出现空指针异常。
+		MockitoAnnotations.initMocks(this);
 	}
 
 	void setInstancePrivateField(Object instance, String fieldname, Object fielvalue) throws NoSuchFieldException, IllegalAccessException {
@@ -45,10 +49,80 @@ public class AuthenticationClientApplicationTests {
 		field.set(instance, fielvalue);
 	}
 
+	// JWT 包含 header.payload.signature
 	@Test
-	public void testInvalidJwtAccessToken_假如授权服务器通过给定的密钥生成token_当输入该token组成的authentication_那么返回false标识token有效(){
+	public void testInvalidJwtAccessToken_假如授权服务器通过给定的密钥生成token_当输入该token组成的authentication_那么返回false表示token有效(){
 		Assert.assertFalse(authService.invalidTokenAccess(BEARER + VALID_TOKEN));
 	}
 
+	@Test
+	public void testInvalidJwtAccessToken_假如token是随机字符串_当输入该token_那么返回true表示token无效(){
+		String authentication = "i'm random token";
+		Assert.assertTrue(authService.invalidTokenAccess(authentication));
+	}
 
+	@Test
+	public void testInvalidJwtAccessToken_假如有人获取了用户的token_当输入篡改过的payload信息_那么返回true表示token无效(){
+		String authentication = BEARER + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.JzaXdhbnBlciIsInNjb3BlIjpbInJlYWQiXSwib3JnYW5pemF0aW9uIjoic2l3YW5wZXIiLCJleHAiOjE1ODg5Mzg1OTQsImF1dGhvcml0aWVzIjpbIklUIl0sImp0aSI6Ijc4MTdkMTI4LTE0NTYtNDdiYy05YzhkLTNiM2JjNDcyYmNlNyIsImNsaWVudF9pZCI6InRlc3RfY2xpZW50In0.NgCOFrL5Xwdp7F8xTEiNfk1cumNIeAxO7tfF8LwUq7k";
+		Assert.assertTrue(authService.invalidTokenAccess(authentication));
+	}
+
+	@Test
+	public void testInvalidJwtAccessToken_假如有人获取了用户token_当输入的token去掉了signature_那么返回true表示token无效(){
+		String authentication = BEARER + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJzaXdhbnBlciIsInNjb3BlIjpbInJlYWQiXSwib3JnYW5pemF0aW9uIjoic2l3YW5wZXIiLCJleHAiOjE1ODg5Mzg1OTQsImF1dGhvcml0aWVzIjpbIklUIl0sImp0aSI6Ijc4MTdkMTI4LTE0NTYtNDdiYy05YzhkLTNiM2JjNDcyYmNlNyIsImNsaWVudF9pZCI6InRlc3RfY2xpZW50In0";
+		Assert.assertTrue(authService.invalidTokenAccess(authentication));
+	}
+
+	@Test
+	public void testAuthenticate_假如用户authenticate正确且有对请求url的POST权限_当用户请求url的POST_那么返回成功有权限(){
+		// 模拟authenticationProvider，请求数据
+		when(authenticationProvider.decide(BEARER + VALID_TOKEN, "/user/add", "POST")).thenReturn(Result.success(true));
+		Assert.assertTrue((Boolean) authService.authentication(BEARER + VALID_TOKEN, "/user/add", "POST").getData());
+	}
+
+	@Test
+	public void testAuthenticate_假如用户authentication正确且有对请求url的POST权限_当用户请求该url的GET_那么返回成功无权限() {
+		// 模拟authenticationProvider，请求数据
+		when(authenticationProvider.decide(BEARER + VALID_TOKEN, "/user/add", "GET")).thenReturn(Result.success(false));
+		Assert.assertFalse((Boolean) authService.authentication(BEARER + VALID_TOKEN, "/user/add", "GET").getData());
+	}
+
+	@Test
+	public void testHasPermission_假如传入无效的token_那么返回无权限(){
+		Assert.assertFalse(authService.hasPermission("invalid token", "/user/add", "POST"));
+	}
+
+	@Test
+	public void testHasPermission_假如用户authenticate正确且有对请求url的POST权限_当用户请求url的POST_那么返回成功有权限(){
+		// 模拟authenticationProvider，请求数据
+		when(authenticationProvider.decide(BEARER + VALID_TOKEN, "/user/add", "POST")).thenReturn(Result.success(true));
+		Assert.assertTrue(authService.hasPermission(BEARER + VALID_TOKEN, "/user/add", "POST"));
+	}
+
+	@Test
+	public void testHasPermission_假如用户authentication正确且有对请求url的POST权限_当用户请求该url的GET_那么返回成功无权限() {
+		// 模拟authenticationProvider，请求数据
+		when(authenticationProvider.decide(BEARER + VALID_TOKEN, "/user/add", "GET")).thenReturn(Result.success(false));
+		Assert.assertFalse( authService.hasPermission(BEARER + VALID_TOKEN, "/user/add", "GET"));
+	}
+
+	@Test
+	public void testIgnoreAuthentication_假如配置的忽略前缀为oauth和open_当用户请求以oauth开头的url_那么返回true(){
+		Assert.assertTrue(authService.ignoreAuthentication("/oauth/decide"));
+	}
+
+	@Test
+	public void testIgnoreAuthentication_假如配置的忽略前缀为oauth和open_当用户请求以open开头的url_那么返回true(){
+		Assert.assertTrue(authService.ignoreAuthentication("/open"));
+	}
+
+	@Test
+	public void testIgnoreAuthentication_假如配置的忽略前缀为oauth和open_当用户请求以test开头的url_那么返回false(){
+		Assert.assertFalse(authService.ignoreAuthentication("/test"));
+	}
+
+	@Test
+	public void testIgnoreAuthentication_假如配置的忽略前缀为oauth和open_当用户请求以open结尾的url_那么返回false(){
+		Assert.assertFalse(authService.ignoreAuthentication("/test/open"));
+	}
 }
